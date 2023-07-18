38f28b46e9425ffefb48a43ea4b9365c1b93821c
qualimock <qualimock@gmail.com>
2023-07-18 15:57:36 +0300

    Reformat code, remove trash

--- qtmpv.h
+++ qtmpv.h
@@ -5,38 +5,35 @@
 
 #include <mpv/client.h>
 
+
 class QTextEdit;
-class QLabel;
-class QPainter;
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+
 private slots:
     void on_file_open();
-    void on_new_window();
     void on_mpv_events();
 
 signals:
     void mpv_events();
 
+
 private:
     QWidget *mpv_container;
     QTextEdit *log;
-    QLabel *label;
-    QPainter *painter;
     mpv_handle *mpv;
 
     void append_log(const QString &text);
-
-    void create_player();
     void handle_mpv_event(mpv_event *event);
-    void paintEvent(QPaintEvent *event);
 };
 
 #endif // QTEXAMPLE_H
