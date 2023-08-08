4928d0ce37eb64ef5bb7c43818617a9a1f70cd5c
Puffy <klimov-danya.04@mail.ru>
2023-08-08 18:15:44 +0300

    Fix bugs

--- installer.bash
+++ installer.bash
@@ -17,7 +17,9 @@ function main {
 	curl https://github.com/QualiMock/qtmpv/releases/download/v0.0.2/qtmpv
 	chmod +x qtmpv
 
-	sudo mv qtmpv /usr/bin/
+	sudo mv qtmpv /opt/
+
+	echo -e "\e[4m\e[1mqtmpv\e[0m установлен в \e[0m\e[32m/opt\e[0m\e[1m. Для запуска пропишите \e[32m/opt/qtmpv\e[0m"
 }
 
 
